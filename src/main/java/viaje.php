8510a4930757cd79a373256948652dbb87bb8f9f
EdwinAB0910 <erab091003@gmail.com>
2025-02-13 16:01:40 -0500

    Add files via upload

---
+++ src/main/java/viaje.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `viaje`
+ */
+
+/* `viaje`.`datosd` */
+$datosd = array(
+  array('CODDATOS' => 'D001','CODD' => '01','FECHA' => '12/04/24','HORA' => '16:00','BUS' => '1','costo' => '180.0'),
+  array('CODDATOS' => 'D002','CODD' => '02','FECHA' => '13/04/24','HORA' => '08:00','BUS' => '2','costo' => '80.0'),
+  array('CODDATOS' => 'D003','CODD' => '03','FECHA' => '13/04/24','HORA' => '18:00','BUS' => '3','costo' => '50.0'),
+  array('CODDATOS' => 'D004','CODD' => '04','FECHA' => '14/04/24','HORA' => '20:00','BUS' => '1','costo' => '75.0'),
+  array('CODDATOS' => 'D005','CODD' => '05','FECHA' => '15/04/24','HORA' => '10:00','BUS' => '2','costo' => '120.0'),
+  array('CODDATOS' => 'D006','CODD' => '06','FECHA' => '15/04/24','HORA' => '22:00','BUS' => '3','costo' => '140.0'),
+  array('CODDATOS' => 'D007','CODD' => '01','FECHA' => '16/04/24','HORA' => '19:00','BUS' => '1','costo' => '180.0'),
+  array('CODDATOS' => 'D008','CODD' => '02','FECHA' => '17/04/24','HORA' => '21:00','BUS' => '2','costo' => '80.0'),
+  array('CODDATOS' => 'D009','CODD' => '03','FECHA' => '18/04/24','HORA' => '18:00','BUS' => '3','costo' => '50.0'),
+  array('CODDATOS' => 'D010','CODD' => '04','FECHA' => '19/04/24','HORA' => '22:00','BUS' => '1','costo' => '75.0'),
+  array('CODDATOS' => 'D011','CODD' => '05','FECHA' => '20/04/24','HORA' => '23:45','BUS' => '2','costo' => '120.0'),
+  array('CODDATOS' => 'D012','CODD' => '06','FECHA' => '21/04/24','HORA' => '18:30','BUS' => '3','costo' => '140.0'),
+  array('CODDATOS' => 'D013','CODD' => '01','FECHA' => '22/04/24','HORA' => '16:40','BUS' => '1','costo' => '180.0'),
+  array('CODDATOS' => 'D014','CODD' => '02','FECHA' => '22/04/24','HORA' => '17:30','BUS' => '2','costo' => '80.0'),
+  array('CODDATOS' => 'D015','CODD' => '03','FECHA' => '22/04/24','HORA' => '20:45','BUS' => '3','costo' => '50.0'),
+  array('CODDATOS' => 'D016','CODD' => '04','FECHA' => '23/04/24','HORA' => '15:20','BUS' => '1','costo' => '75.0'),
+  array('CODDATOS' => 'D017','CODD' => '05','FECHA' => '23/04/24','HORA' => '19:50','BUS' => '2','costo' => '120.0'),
+  array('CODDATOS' => 'D018','CODD' => '06','FECHA' => '24/04/24','HORA' => '18:10','BUS' => '3','costo' => '140.0'),
+  array('CODDATOS' => 'D019','CODD' => '01','FECHA' => '25/04/24','HORA' => '19:25','BUS' => '1','costo' => '180.0'),
+  array('CODDATOS' => 'D020','CODD' => '02','FECHA' => '26/04/24','HORA' => '23:30','BUS' => '2','costo' => '80.0'),
+  array('CODDATOS' => 'D021','CODD' => '03','FECHA' => '27/04/24','HORA' => '18:20','BUS' => '3','costo' => '50.0'),
+  array('CODDATOS' => 'D022','CODD' => '04','FECHA' => '28/04/24','HORA' => '22:00','BUS' => '1','costo' => '75.0'),
+  array('CODDATOS' => 'D023','CODD' => '05','FECHA' => '29/04/24','HORA' => '13:00','BUS' => '2','costo' => '120.0'),
+  array('CODDATOS' => 'D024','CODD' => '06','FECHA' => '30/04/24','HORA' => '09:00','BUS' => '3','costo' => '140.0')
+);
+
+/* `viaje`.`destino` */
+$destino = array(
+  array('CODD' => '01','NOMbre' => 'Arequipa'),
+  array('CODD' => '02','NOMbre' => 'Huanuco'),
+  array('CODD' => '03','NOMbre' => 'Ica'),
+  array('CODD' => '04','NOMbre' => 'Trujillo'),
+  array('CODD' => '05','NOMbre' => 'Piura'),
+  array('CODD' => '06','NOMbre' => 'Tumbes')
+);
+
+/* `viaje`.`pasajero` */
+$pasajero = array(
+  array('NROBOLETO' => '000001','CODDATOS' => 'D001','NOMBRE' => 'Yamila Avellaneda','TIPO' => '1','PAGO' => '80.0'),
+  array('NROBOLETO' => '000002','CODDATOS' => 'D002','NOMBRE' => 'Manuel Barranca','TIPO' => '2','PAGO' => '60.0'),
+  array('NROBOLETO' => '000003','CODDATOS' => 'D003','NOMBRE' => 'Miriam Yery','TIPO' => '3','PAGO' => '50.0'),
+  array('NROBOLETO' => '000004','CODDATOS' => 'D004','NOMBRE' => 'Fernando Tabarez','TIPO' => '1','PAGO' => '100.0'),
+  array('NROBOLETO' => '000005','CODDATOS' => 'D005','NOMBRE' => 'Timo Sanchez','TIPO' => '2','PAGO' => '50.0'),
+  array('NROBOLETO' => '000006','CODDATOS' => 'D006','NOMBRE' => 'Facundo Racho','TIPO' => '3','PAGO' => '30.0'),
+  array('NROBOLETO' => '000007','CODDATOS' => 'D007','NOMBRE' => 'Maria Avila','TIPO' => '1','PAGO' => '120.0'),
+  array('NROBOLETO' => '000008','CODDATOS' => 'D008','NOMBRE' => 'Yomar Ureta','TIPO' => '2','PAGO' => '100.0'),
+  array('NROBOLETO' => '000009','CODDATOS' => 'D009','NOMBRE' => 'Francisco Villa','TIPO' => '3','PAGO' => '70.0'),
+  array('NROBOLETO' => '000010','CODDATOS' => 'D010','NOMBRE' => 'Magaly Pica','TIPO' => '1','PAGO' => '75.0'),
+  array('NROBOLETO' => '000011','CODDATOS' => 'D011','NOMBRE' => 'Raul Asencio','TIPO' => '2','PAGO' => '65.0'),
+  array('NROBOLETO' => '000012','CODDATOS' => 'D012','NOMBRE' => 'Mario Villanueva','TIPO' => '3','PAGO' => '55.0'),
+  array('NROBOLETO' => '000013','CODDATOS' => 'D013','NOMBRE' => 'Ana Sira','TIPO' => '1','PAGO' => '45.0'),
+  array('NROBOLETO' => '000014','CODDATOS' => 'D014','NOMBRE' => 'Domingo Tito','TIPO' => '2','PAGO' => '85.0'),
+  array('NROBOLETO' => '000015','CODDATOS' => 'D015','NOMBRE' => 'Paolo Chumpi','TIPO' => '3','PAGO' => '67.0'),
+  array('NROBOLETO' => '000016','CODDATOS' => 'D016','NOMBRE' => 'Federico Garcia','TIPO' => '1','PAGO' => '84.0'),
+  array('NROBOLETO' => '000017','CODDATOS' => 'D017','NOMBRE' => 'Samuel Pijuan','TIPO' => '2','PAGO' => '45.0'),
+  array('NROBOLETO' => '000018','CODDATOS' => 'D018','NOMBRE' => 'Paola Ricra','TIPO' => '3','PAGO' => '65.0'),
+  array('NROBOLETO' => '000019','CODDATOS' => 'D019','NOMBRE' => 'Miguel Gonzales','TIPO' => '1','PAGO' => '55.0'),
+  array('NROBOLETO' => '000020','CODDATOS' => 'D020','NOMBRE' => 'Fabian Hureña','TIPO' => '2','PAGO' => '89.0'),
+  array('NROBOLETO' => '000021','CODDATOS' => 'D021','NOMBRE' => 'Martin Quispe','TIPO' => '3','PAGO' => '45.0'),
+  array('NROBOLETO' => '000022','CODDATOS' => 'D022','NOMBRE' => 'Alberto Cueva','TIPO' => '1','PAGO' => '74.0'),
+  array('NROBOLETO' => '000023','CODDATOS' => 'D023','NOMBRE' => 'Fabiana Lance','TIPO' => '2','PAGO' => '65.0'),
+  array('NROBOLETO' => '000024','CODDATOS' => 'D024','NOMBRE' => 'Tertulia Jacobo','TIPO' => '3','PAGO' => '60.0'),
+  array('NROBOLETO' => '000027','CODDATOS' => 'D003','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '102.0'),
+  array('NROBOLETO' => '000028','CODDATOS' => 'D004','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '89.0'),
+  array('NROBOLETO' => '000029','CODDATOS' => 'D005','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '58.0'),
+  array('NROBOLETO' => '000030','CODDATOS' => 'D006','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '67.0'),
+  array('NROBOLETO' => '000031','CODDATOS' => 'D007','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '95.0'),
+  array('NROBOLETO' => '000032','CODDATOS' => 'D008','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '45.0'),
+  array('NROBOLETO' => '000033','CODDATOS' => 'D009','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '100.0'),
+  array('NROBOLETO' => '000034','CODDATOS' => 'D010','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '110.0'),
+  array('NROBOLETO' => '000035','CODDATOS' => 'D011','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '120.0'),
+  array('NROBOLETO' => '000036','CODDATOS' => 'D012','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '85.0'),
+  array('NROBOLETO' => '000037','CODDATOS' => 'D013','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '65.0'),
+  array('NROBOLETO' => '000038','CODDATOS' => 'D014','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '78.0'),
+  array('NROBOLETO' => '000039','CODDATOS' => 'D015','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '95.0'),
+  array('NROBOLETO' => '000040','CODDATOS' => 'D016','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '75.0'),
+  array('NROBOLETO' => '000041','CODDATOS' => 'D017','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '30.0'),
+  array('NROBOLETO' => '000042','CODDATOS' => 'D018','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '86.0'),
+  array('NROBOLETO' => '000043','CODDATOS' => 'D019','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '76.0'),
+  array('NROBOLETO' => '000044','CODDATOS' => 'D020','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '95.0'),
+  array('NROBOLETO' => '000045','CODDATOS' => 'D021','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '64.0'),
+  array('NROBOLETO' => '000046','CODDATOS' => 'D022','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '45.0'),
+  array('NROBOLETO' => '000047','CODDATOS' => 'D023','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '100.0'),
+  array('NROBOLETO' => '000048','CODDATOS' => 'D024','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '106.0'),
+  array('NROBOLETO' => '000049','CODDATOS' => 'D001','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '104.0'),
+  array('NROBOLETO' => '000050','CODDATOS' => 'D002','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '57.0'),
+  array('NROBOLETO' => '000051','CODDATOS' => 'D003','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '95.0'),
+  array('NROBOLETO' => '000052','CODDATOS' => 'D004','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '100.0'),
+  array('NROBOLETO' => '000053','CODDATOS' => 'D005','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '65.0'),
+  array('NROBOLETO' => '000054','CODDATOS' => 'D006','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '79.0'),
+  array('NROBOLETO' => '000055','CODDATOS' => 'D007','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '85.0'),
+  array('NROBOLETO' => '000056','CODDATOS' => 'D008','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '100.0'),
+  array('NROBOLETO' => '000057','CODDATOS' => 'D009','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '110.0'),
+  array('NROBOLETO' => '000058','CODDATOS' => 'D010','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '84.0'),
+  array('NROBOLETO' => '000059','CODDATOS' => 'D011','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '65.0'),
+  array('NROBOLETO' => '000060','CODDATOS' => 'D012','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '85.0'),
+  array('NROBOLETO' => '000061','CODDATOS' => 'D013','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '95.0'),
+  array('NROBOLETO' => '000062','CODDATOS' => 'D014','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '100.0'),
+  array('NROBOLETO' => '000063','CODDATOS' => 'D015','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '101.0'),
+  array('NROBOLETO' => '000064','CODDATOS' => 'D016','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '106.0'),
+  array('NROBOLETO' => '000065','CODDATOS' => 'D017','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '102.0'),
+  array('NROBOLETO' => '000066','CODDATOS' => 'D018','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '90.0'),
+  array('NROBOLETO' => '000067','CODDATOS' => 'D019','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '80.0'),
+  array('NROBOLETO' => '000068','CODDATOS' => 'D020','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '70.0'),
+  array('NROBOLETO' => '000069','CODDATOS' => 'D021','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '60.0'),
+  array('NROBOLETO' => '000070','CODDATOS' => 'D022','NOMBRE' => 'Garcia Avellaneda','TIPO' => '1','PAGO' => '80.0'),
+  array('NROBOLETO' => '000071','CODDATOS' => 'D023','NOMBRE' => 'Garcia Avellaneda','TIPO' => '2','PAGO' => '95.0'),
+  array('NROBOLETO' => '000072','CODDATOS' => 'D024','NOMBRE' => 'Garcia Avellaneda','TIPO' => '3','PAGO' => '84.0')
+);
